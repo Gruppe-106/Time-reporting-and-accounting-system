4a4a7632de2e98f073f2411f86472a49b84df9c1
Andreas <ahumme22@student.aau.dk>
2023-03-13 12:38:23 +0100

    String test added

--- src/tests/example.test.ts
+++ src/tests/example.test.ts
@@ -15,13 +15,13 @@ test('Example of different tests', () => {
     function DevideByZero(){
         Devide(1,0)
     }
-    
+
     expect(Sum(1, 2)).toBe(3);
     expect(0).not.toBeTruthy()
     expect(0).toBeFalsy()
 
     expect(Devide(4,2)).toBe(2)
-
+    expect("Computer Science").toMatch(/^Computer Science$/)
     // Test that the error message says "Devide" somewhere: these are equivalent
     expect(DevideByZero).toThrow(/error/);
     expect(DevideByZero).toThrow('error');
