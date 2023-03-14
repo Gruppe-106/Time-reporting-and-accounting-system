b2e51e93d58431d63849210cbae90e9697140997
madsbyriel <madshbyriel@gmail.com>
2023-03-14 11:32:53 +0100

    Created dummy data for the UserModel! Go use the async to simulate database calls.

--- src/client/src/Models/employeeModel.ts
+++ src/client/src/Models/employeeModel.ts
@@ -1,4 +1,5 @@
 import BaseModel from "./baseModel";
+import {ProjectData} from "./projectModel";
 
 interface UserData {
     id:number;
@@ -9,8 +10,65 @@ interface UserData {
 }
 
 class UserModel extends BaseModel<UserData> {
-    GetAllModels(): Promise<UserData[]> {
-        throw (new Error("Not implemented"));
+    public static idCounterDeleteMe:number = 0;
+
+    public GetAllModels(): Promise<UserData[]> {
+        return new Promise<UserData[]>(async (resolve, reject) => {
+            let u1: UserData = {
+                id: UserModel.idCounterDeleteMe++,
+                email: "@gmail.com",
+                firstName: "",
+                lastName: "",
+                group: 0,
+            }
+            let u2: UserData = {
+                id: UserModel.idCounterDeleteMe++,
+                email: "alexander@gmail.com",
+                firstName: "Alexander",
+                lastName: "Guldberg",
+                group: 0,
+            }
+            let u3: UserData = {
+                id: UserModel.idCounterDeleteMe++,
+                email: "christian@gmail.com",
+                firstName: "Christan",
+                lastName: "Bonde",
+                group: 0,
+            }
+            let u4: UserData = {
+                id: UserModel.idCounterDeleteMe++,
+                email: "simon@gmail.com",
+                firstName: "Simon",
+                lastName: "Nielsen",
+                group: 0,
+            }
+            let u5: UserData = {
+                id: UserModel.idCounterDeleteMe++,
+                email: "mikkel@gmail.com",
+                firstName: "Mikkel",
+                lastName: "Helsing",
+                group: 0,
+            }
+            let u6: UserData = {
+                id: UserModel.idCounterDeleteMe++,
+                email: "hvid@gmail.com",
+                firstName: "Hvid",
+                lastName: "Byriel",
+                group: 0,
+            }
+            let u7: UserData = {
+                id: UserModel.idCounterDeleteMe++,
+                email: "mads@gmail.com",
+                firstName: "Mads",
+                lastName: "Kjerulff",
+                group: 0,
+            }
+            let uDataList = [
+                u1, u2, u3, u4, u5, u6, u7,
+            ]
+
+            await setTimeout(() => {resolve(uDataList)}, 3000);
+        });
     }
 
     GetModelById(id: number): Promise<UserData> {
