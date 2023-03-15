b9a0f8dd7672b3f61481f199f007ff43feb48eaa
Christianbonde <cbje22@student.aau.dk>
2023-03-15 12:18:26 +0100

    New userTimeRegister

--- src/client/src/pages/timeRegister/componentsTime/com.tsx
+++ src/client/src/pages/timeRegister/componentsTime/com.tsx
@@ -1,56 +1,13 @@
-import {Container, Table, Form} from "react-bootstrap";
+import {Container, Table, Form, Dropdown, DropdownButton, InputGroup} from "react-bootstrap";
 import React, {Component} from "react";
 
-interface TimeTableProp{
-    tableRows:TimeTableRow[]
-}
-
-interface TimeTableRow{
-    projectName:string
-    taskName:string
-    totalTime:number
-}
-
-class TimeTableRegister extends Component<TimeTableProp> {
-    tableRows:TimeTableRow[];
-
-    constructor(props:TimeTableProp) {
+class TimeTableRegister extends Component<any> {
+    constructor(props:any) {
         super(props);
-        this.tableRows = props.tableRows;
-    }
-
-    private tableRender():JSX.Element[]{
-        return this.tableRows.map((row) =>{
-
-            return (<tr><td>{row.projectName}</td>
-                        <td>{row.taskName}</td>
-                        <td><Form><Form.Group controlId="exampleForm.ControlInput1">
-                            <Form.Control type="number" value={0} />
-                        </Form.Group></Form></td>
-                        <td><Form><Form.Group controlId="exampleForm.ControlInput2">
-                            <Form.Control type="number" value={0} />
-                        </Form.Group></Form></td>
-                        <td><Form><Form.Group controlId="exampleForm.ControlInput3">
-                            <Form.Control type="number" value={0} />
-                        </Form.Group></Form></td>
-                        <td><Form><Form.Group controlId="exampleForm.ControlInput4">
-                            <Form.Control type="number" value={0} />
-                        </Form.Group></Form></td>
-                        <td><Form><Form.Group controlId="exampleForm.ControlInput5">
-                            <Form.Control type="number" value={0} />
-                        </Form.Group></Form></td>
-                        <td><Form><Form.Group controlId="exampleForm.ControlInput6">
-                            <Form.Control type="number" value={0} />
-                        </Form.Group></Form></td>
-                        <td><Form><Form.Group controlId="exampleForm.ControlInput7">
-                            <Form.Control type="number" value={0} />
-                        </Form.Group></Form></td>
-                        <td>{row.totalTime}</td>
-                    </tr>)
-        })
     }
 
     render() {
+        let arr = ['1', '2', '3', '4', '5', '6', '7'];
         return(
             <Container>
                 <Table bordered hover size="sm">
@@ -58,18 +15,32 @@ class TimeTableRegister extends Component<TimeTableProp> {
                 <tr>
                     <th>Project Name</th>
                     <th>Task Name</th>
-                    <th>1 Date</th>
-                    <th>2 Date</th>
-                    <th>3 Date</th>
-                    <th>4 Date</th>
-                    <th>5 Date</th>
-                    <th>6 Date</th>
-                    <th>7 Date</th>
+                    {arr.map((va) => (<th>{va} Date</th>))}
                     <th>Total Time</th>
                 </tr>
                 </thead>
                 <tbody>
-                {this.tableRender()}
+                <tr>
+                <td>ExampleProject</td>
+                <td>ExampleTask</td>
+                {arr.map(
+                (value) => (
+                <td><InputGroup>
+                    <Form.Control type="number" placeholder="0" />
+                    <DropdownButton
+                    variant="secondary"
+                    title="0"
+                    id={`time-input-${value}`}
+                    align="end"
+                    >
+                    <Dropdown.Item eventKey={1}>15</Dropdown.Item>
+                    <Dropdown.Item eventKey={2}>30</Dropdown.Item>
+                    <Dropdown.Item eventKey={3}>45</Dropdown.Item>
+                    <Dropdown.Item eventKey={4}>60</Dropdown.Item>
+                    </DropdownButton>
+                </InputGroup></td>))}
+                <td>0</td>
+                </tr>
                 <tr><th colSpan={2}>Total Time:</th></tr>
                 </tbody>
                 </Table>
