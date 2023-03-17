470c59c282aeec722e3b67d416cc85f0ffb5fb30
Oragif <Mikkelhelsing1@gmail.com>
2023-03-17 18:26:23 +0100

    Added endpoint for user creator, data isn't used currently

--- src/client/src/pages/adminPanel/adminPanel.tsx
+++ src/client/src/pages/adminPanel/adminPanel.tsx
@@ -8,18 +8,11 @@
 
 import React, {Component} from "react";
 import BaseNavBar from "../../components/navBar";
-import {Button, Container} from "react-bootstrap";
-import {Buffer} from "buffer";
-import BaseApiHandler from "../../network/baseApiHandler";
+import {Container} from "react-bootstrap";
 
 class AdminPanel extends Component<any>{
     constructor(props:any) {
         super(props);
-    }
-
-    private getData() {
-        let apiHandler = new BaseApiHandler("test");
-        apiHandler.post("/api/user/creation/post", (value) => {}, undefined, {test: "test"})
     }
 
     render() {
@@ -28,7 +21,6 @@ class AdminPanel extends Component<any>{
                 <BaseNavBar/>
                 <Container>
                     <h1>Admin</h1>
-                    <Button onClick={this.getData}>Post test</Button>
                 </Container>
             </>
         );
