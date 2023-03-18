923ddf422f22b55498ff3263262e702bcffdef7e
Oragif <Mikkelhelsing1@gmail.com>
2023-03-18 16:51:45 +0100

    Comments

--- src/server/database/mysqlHandler.ts
+++ src/server/database/mysqlHandler.ts
@@ -15,10 +15,17 @@ class MysqlHandler {
         this.hasOrCreateConnection();
     }
 
+    /**
+     * @return Boolean: Returns whether a MySQL connection is present or not
+     */
     public hasConnection(): boolean {
         return MysqlHandler.connection !== undefined;
     }
 
+    /** Tries to create a connection to a MySQL database if one doesn't exist
+     * @return Boolean: Returns whether a MySQL connection is present or not
+     * @private
+     */
     private createConnection(): boolean {
         if (MysqlHandler.connectionConfig !== undefined) {
             MysqlHandler.connection = mysql.createConnection(MysqlHandler.connectionConfig);
@@ -35,6 +42,9 @@ class MysqlHandler {
         return false;
     }
 
+    /** Checks if connection is present, if not try to create one
+     * @return Boolean: Returns true if connection is present otherwise false
+     */
     public hasOrCreateConnection(): boolean {
         if (!this.hasConnection()) {
             return this.createConnection();
@@ -42,21 +52,46 @@ class MysqlHandler {
         return true;
     }
 
+    /**
+     * Stops connection to the mysql server
+     */
     public destroyConnection(): void {
-        MysqlHandler.connection.destroy();
-        console.log("[MySQL] connection to database destroyed");
+        if (this.hasConnection()) {
+            MysqlHandler.connection.destroy();
+            return console.log("[MySQL] connection to database destroyed");
+        }
+        console.log("[MySQL] Can't stop connection to DB as there is none")
     }
 
+    /**
+     * Creates a string with the sql query condition
+     * @param where Where: condition includes column to check and what to check against
+     * @return String: of the condition or an empty string if where is empty
+     * @private
+     */
     private createWhereString(where?:Where): string {
         return where !== undefined ? "WHERE " + where.column + " in (" + where.equals.toString() + ")" : "";
     }
 
+    /**
+     * Sends a query to the database if one is present
+     * @param sqlQuery String: query to send to server
+     * @param callback Callback: callback function to process data
+     * @private
+     */
     private sendQuery(sqlQuery: string, callback?: (error: MysqlError | null, results: any, fields: FieldInfo[]) => void): void {
         if (this.hasOrCreateConnection()) {
             MysqlHandler.connection.query({ sql: sqlQuery, timeout: 30000 }, callback);
         }
     }
 
+    /**
+     * Sends a select query to server
+     * @param table String: table to get data from
+     * @param columns String[]?: column(s) to retrieve
+     * @param where Where?: condition for the selection of data
+     * @param callback Callback: callback function to process data
+     */
     public select(table: string, columns?: string[], where?:Where, callback?: (error: MysqlError | null, results: any, fields: FieldInfo[]) => void): void {
         let queryString = `SELECT ${columns !== undefined ? columns : "*"} FROM ${table} ${this.createWhereString(where)}`;
         this.sendQuery(queryString, callback);
