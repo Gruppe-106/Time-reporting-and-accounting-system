1012ded8d55a9d1f035e9aa213de6910620bdbc2
Mikkel Andersen <108178976+Oragif@users.noreply.github.com>
2023-03-18 16:25:13 +0100

    Hot fix, this commit was not suppose to go to main
    Signed-off-by: Mikkel Andersen <108178976+Oragif@users.noreply.github.com>

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,22 +1,18 @@
 import {Express} from "express";
 import {MainRouter} from "./mainRouter";
-import * as https from "https";
 
 export class Server {
     private app: Express;
     private router: MainRouter
-    private server: https.Server;
 
     constructor(app: Express) {
         this.app = app;
         this.router = new MainRouter();
         app.use('/', this.router.routes());
-
-        this.server = https.createServer(app);
     }
 
     public start(port: number): void {
-        this.server.listen(port, () => console.log(`[HTTP] Server listening on port ${port}, http://localhost:${port}`));
+        this.app.listen(port, () => console.log(`[HTTP] Server listening on port ${port}, http://localhost:${port}`));
     }
 
     public stop(): void {
