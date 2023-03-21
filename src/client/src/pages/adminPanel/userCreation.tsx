8a834b71ee2480922483e995a30874574f77578a
Andreas <ahumme22@student.aau.dk>
2023-03-21 10:02:09 +0100

    Splitting up the files

--- src/client/src/pages/adminPanel/userCreation.tsx
+++ src/client/src/pages/adminPanel/userCreation.tsx
@@ -3,22 +3,23 @@
 */
 
 //React imports
-import React, { Component } from "react";
-import { Container, Modal } from "react-bootstrap";
 import BaseNavBar from "../../components/navBar";
 import Button from 'react-bootstrap/Button';
-import Spinner from 'react-bootstrap/Spinner';
+import { Container, Modal } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
-import { Typeahead, Highlighter } from 'react-bootstrap-typeahead';
-import 'react-bootstrap-typeahead/css/Typeahead.css';
 import LoadingOverlay from 'react-loading-overlay-ts';
+import React, { Component } from "react";
+import { Highlighter, Typeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import Spinner from 'react-bootstrap/Spinner';
 
 //Forge import
 import forge from 'node-forge';
 
 //Custom import
 import BaseApiHandler from "../../network/baseApiHandler";
-
+import Utility from './utility/userCreation/userCreation'
+import APICalls from "./utility/userCreation/apiCalls";
 
 //TODO: fixings the types as this is infact a no no but it does fix it
 
@@ -50,184 +51,6 @@ interface CustomTypes {
     popupMessage: string,
     popupTitle: string,
     loadingText: string
-
-}
-
-
-/**
- * Class containing utility methods
-*/
-class Utility {
-
-    /**
-     * Method to validate fields
-     * @param userObject The object containing the current field information
-     * @returns Object containing information about missings fields if any
-     */
-    public static CheckFields(userObject: {
-        [key: string]: any
-        firstName: string | null,
-        lastName: string | null,
-        email: string | null,
-        password: string | null,
-        assignedToManager: { roleName: string, roleId: number, userId: number, firstName: string, lastName: string } | null
-        roles: { id: number, name: string }[] | null
-    }): {
-        valid: boolean,
-        missingFields: number,
-        errorString: string
-    } {
-        let missing: string[] = []
-        let keys: string[] = Object.keys(userObject)
-        let missingString: string = "";
-        let valid: boolean = true
-
-        for (let i = 0; i < keys.length; i++) {
-
-            if (userObject[keys[i]] === null) {
-                missing.push(keys[i])
-            }
-        }
-
-        if (missing.length > 0) {
-
-            valid = false;
-            let split: string = ""
-
-            if (missing.length === 1) {
-
-                if (missing[0] === "firstName") {
-                    split = "First name"
-                } else if (missing[0] === "lastName") {
-                    split = "Last name"
-                } else if (missing[0] === "email") {
-                    split = "Email address"
-                } else if (missing[0] === "password") {
-                    split = "Password"
-                } else if (missing[0] === "assignedToManager") {
-                    split = "Assign manager"
-                } else if (missing[0] === "roles") {
-                    split = "Assign roles"
-                }
-
-
-                missingString += "Missing field: " + split
-
-            } else if (missing.length > 1) {
-
-
-                missingString += "Missing fields: "
-
-                for (let i = 0; i < missing.length - 1; i++) {
-
-                    if (missing[i] === "firstName") {
-                        split = "First name"
-                    } else if (missing[i] === "lastName") {
-                        split = "Last name"
-                    } else if (missing[i] === "email") {
-                        split = "Email address"
-                    } else if (missing[i] === "password") {
-                        split = "Password"
-                    } else if (missing[i] === "assignedToManager") {
-                        split = "Assign manager"
-                    } else if (missing[i] === "roles") {
-                        split = "Assign roles"
-                    }
-                    if (split) {
-                        missingString += split + ", "
-                    }
-                    split = ""
-                }
-
-                missingString += " and "
-
-                if (missing[missing.length - 1] === "firstName") {
-                    split = "First name"
-                } else if (missing[missing.length - 1] === "lastName") {
-                    split = "Last name"
-                } else if (missing[missing.length - 1] === "email") {
-                    split = "Email address"
-                } else if (missing[missing.length - 1] === "password") {
-                    split = "Password"
-                } else if (missing[missing.length - 1] === "assignedToManager") {
-                    split = "Assign manager"
-                } else if (missing[missing.length - 1] === "roles") {
-                    split = "Assign roles"
-                }
-                missingString += split
-            }
-
-        }
-
-        return {
-            valid: valid,
-            missingFields: missing.length,
-            errorString: missingString
-        }
-    }
-
-}
-
-/**
- * Class containing methods for getting data from the server+9
-*/
-class APICalls {
-
-    /**
-     * Get all roles from the database
-     * @returns Promise containing all possible roles
-    */
-    public static getAllRoles(): Promise<{ status: number, data: { id: number, name: string }[] }> {
-        // const apiHandler:BaseApiHandler = new BaseApiHandler()
-
-        return fetch(`/api/role/get?ids=*`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((response: Response) => {
-                if (response.status === 400) {
-                    throw new Error("Status 400 bad request")
-                } else if (response.status === 200) {
-                    return response.json()
-                } else {
-                    throw new Error(`Unexpected response status: ${response.status}`)
-                }
-            })
-            .catch(error => {
-                throw new Error(error.Code);
-            });
-
-    }
-
-    /**
-     * Get all roles from the database
-     * @returns Promise containing all possible roles
-    */
-    public static getAllManagers(): Promise<{ id: number, name: string }[]> {
-        // const apiHandler:BaseApiHandler = new BaseApiHandler()
-
-        return fetch(`/api/role/user/get?role=1`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then((response: Response) => {
-                if (response.status === 400) {
-                    throw new Error("Status 400 bad request")
-                } else if (response.status === 200) {
-                    return response.json()
-                } else {
-                    throw new Error(`Unexpected response status: ${response.status}`)
-                }
-            })
-            .catch(error => {
-                throw new Error(error.Code);
-            });
-
-    }
 
 }
 
@@ -513,7 +336,6 @@ class UserCreation extends Component<any, CustomTypes>{
                     loadingText: message || ""
                 })
         } else {
-            console.log("sdasdsad")
             this.setState({ loading: false })
         }
     }
