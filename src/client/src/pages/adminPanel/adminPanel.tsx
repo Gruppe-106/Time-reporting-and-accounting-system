e30c0a3cc97a9c92d37d887bb7c669f32999b334
Andreas-Hum <heyoallos@gmail.com>
2023-03-22 11:51:02 +0100

    beginning on admin panal

--- src/client/src/pages/adminPanel/adminPanel.tsx
+++ src/client/src/pages/adminPanel/adminPanel.tsx
@@ -6,22 +6,85 @@
     ...
 */
 
-import React, {Component} from "react";
-import BaseNavBar from "../../components/navBar";
-import {Container} from "react-bootstrap";
+/*
+    Form for creating new users
+*/
 
-class AdminPanel extends Component<any>{
-    constructor(props:any) {
+//React imports
+import BaseNavBar from "../../components/navBar";
+import Button from 'react-bootstrap/Button';
+import { Container, Modal } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import LoadingOverlay from 'react-loading-overlay-ts';
+import React, { Component } from "react";
+import { Highlighter, Typeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import Spinner from 'react-bootstrap/Spinner';
+
+//Forge import
+import forge from 'node-forge';
+
+//Custom import
+import BaseApiHandler from "../../network/baseApiHandler";
+import Utility from './utility/userCreation/userCreation'
+import APICalls from "./utility/userCreation/apiCalls";
+
+
+/**
+ * Custom types
+ */
+interface CustomTypes {
+    // * Input variables
+    firstName: string | null,
+    lastName: string | null,
+    email: string | null,
+    password: string | null,
+    assignedToManager: { roleName: string, roleId: number, userId: number, firstName: string, lastName: string } | null,
+    selectedRoles: { id: number, name: string }[] | null,
+
+    // * Database variables
+    dbRoles: any[],
+    dbManagers: any[],
+
+    // * Input validation
+    emailValid: boolean,
+
+    // * Controlling components
+    submitDisabled: boolean,
+    showPopup: boolean,
+    loading: boolean,
+
+    // * Component variables
+    popupMessage: string,
+    popupTitle: string,
+    loadingText: string
+
+}
+
+class AdminPanel extends Component<any,CustomTypes>{
+    constructor(props: any) {
         super(props);
     }
 
     render() {
         return (
-            <>
-                <BaseNavBar/>
+            <><LoadingOverlay
+                active={this.state.loading}
+                spinner
+                text={this.state.loadingText}
+                styles={{
+                    wrapper: {
+                        height: '100%',
+                        width: '100%'
+                    }
+                }}
+            >
+                <BaseNavBar />
                 <Container>
                     <h1>Admin</h1>
                 </Container>
+
+            </LoadingOverlay>
             </>
         );
     }
