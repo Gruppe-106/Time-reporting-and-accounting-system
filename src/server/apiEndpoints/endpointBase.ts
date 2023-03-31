7b9b6ba920c5dc1f04d30ff925b7e33df0dc41dd
Oragif <Mikkelhelsing1@gmail.com>
2023-03-31 17:15:53 +0200

    Comments

--- src/server/apiEndpoints/endpointBase.ts
+++ src/server/apiEndpoints/endpointBase.ts
@@ -2,12 +2,6 @@ import {Server} from "../server";
 import {Request, Response} from "express";
 import AuthEndpoint, {AuthData} from "./dataGetEndpoints/authEndpoint";
 
-export interface User {
-    authKey: string;
-    id?:     number;
-    role?:   Roles[];
-}
-
 enum Roles {
     NORMAL,
     MANAGER,
@@ -19,17 +13,30 @@ abstract class EndpointBase {
     protected readonly mySQL = Server.mysql;
     abstract requiredRole: number;
 
+    /**
+     * Ensures the user is authorised to access page or api
+     * @param req Request: request object of the re3quest
+     * @protected
+     * @return Boolean: whether the user is allowed
+     */
     protected async ensureAuth(req: Request): Promise<boolean> {
         try {
+            // Get authentication data
             let authData: AuthData = await new AuthEndpoint().getAuthentication(req);
+            // If all users have access and the user has been authenticated return true
             if (this.requiredRole === 1 && authData.userId) return true;
-
+            // Otherwise check if the role match or user is administrator
             let roles: boolean[] = authData.userRoles.map((value) => { return value.roleId === this.requiredRole || value.roleId === Roles.ADMIN } );
             return roles.indexOf(true) !== -1;
         } catch (e) { }
         return false;
     }
 
+    /**
+     * Sends bad request back to the request and ends connection
+     * @param res Response: response object of the api request
+     * @protected
+     */
     protected badRequest(res: Response) {
         res.sendStatus(400);
         res.end();
