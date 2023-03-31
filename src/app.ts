5872e53d87388d0489f261fa257fdc0893bc7f14
Oragif <Mikkelhelsing1@gmail.com>
2023-03-31 18:08:50 +0200

    Fixed import of mysqlConnectionConfig.json

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import {Server} from "./server/server";
 import express from "express";
 import {mysqlCallback} from "./server/database/fakeData/addFakeDataToDB";
+import * as fs from "fs";
 
 export interface MySQLConfig {
     host    : string,
@@ -12,7 +13,7 @@ export interface MySQLConfig {
 // --- Config ---
 const app = express();
 const port = 8080;
-const mySQLConnectionConfig: MySQLConfig = require("mysqlConnectionConfig.json");
+const mySQLConnectionConfig: MySQLConfig = JSON.parse(fs.readFileSync("mysqlConnectionConfig.json", {encoding: "utf-8"}));
 
 // Startup Server
 export const server: Server = new Server(app, mySQLConnectionConfig, port, mysqlCallback);
