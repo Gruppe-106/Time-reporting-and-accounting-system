6e92c093db5ebbc50a0829be954369d793cad83a
KanzanKohi <agredder@gmail.com>
2023-04-13 14:05:43 +0200

    fixed some validation. Still need to add so tasks gets sent aswell

--- src/client/src/pages/projectCreation/components/projectCreate.tsx
+++ src/client/src/pages/projectCreation/components/projectCreate.tsx
@@ -3,7 +3,8 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
-import {Button, Form} from "react-bootstrap";
+import Modal from 'react-bootstrap/Modal'
+import {Alert, Button, Form} from "react-bootstrap";
 import BaseApiHandler from "../../../network/baseApiHandler";
 import {Highlighter, Typeahead} from "react-bootstrap-typeahead";
 import ProjectCreateTask from "./projectCreateTasks";
@@ -29,20 +30,29 @@ interface ProjectCreateProp {
 }
 
 class ProjectCreate extends Component<ProjectCreateProp> {
+    // @ts-ignore
     state = {
         assignedToManager: {"id": Infinity, "name": "" },
         selectedParentProject: {"id": Infinity, "name": ""},
-        pageInformation: [{id: -1, superProject: -1, name: "", startDate: "", endDate: ""}]
+        pageInformation: [{id: -1, superProject: -1, name: "", startDate: "", endDate: ""}],
+        tasks: [],
+        show: false,
+        formSubmitted: false,
+        invalidStartDate: false,
+        invalidEndDate: false
     }
 
     constructor(props:ProjectCreateProp) {
         super(props);
         this.HandleManager = this.HandleManager.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+        this.handleShow = this.handleShow.bind(this)
+        this.handleFormSubmit = this.handleFormSubmit.bind(this)
     }
 
     componentDidMount() {
         //First make an instance of the api handler, give it the auth key of the user once implemented
-        let apiHandler = new BaseApiHandler("test");
+        let apiHandler = new BaseApiHandler();
         //Run the get or post function depending on need only neccesarry argument is the path aka what comes after the hostname
         //Callbacks can be used to tell what to do with the data once it's been retrieved
         apiHandler.get(`/api/project/get?ids=*`, {},(value) => {
@@ -52,6 +62,18 @@ class ProjectCreate extends Component<ProjectCreateProp> {
             //Then update states or variables or whatever you want with the information
             this.setState({pageInformation: json.data})
             console.log(json.data)
+        })
+    }
+
+    private handleClose(){
+        this.setState({
+            show: false
+        });
+    }
+
+    private handleShow(){
+        this.setState({
+            show: true
         })
     }
 
@@ -66,6 +88,34 @@ class ProjectCreate extends Component<ProjectCreateProp> {
         });
     }
 
+    private handleEndDate(){
+        const startDate = new Date((document.getElementById("formBasicStartDate") as HTMLInputElement).value)
+        const endDate = new Date((document.getElementById("formBasicEndDate") as HTMLInputElement).value)
+        let maxDate: Date = new Date("3000-01-01")
+        let minDate: Date = new Date("1970-01-01")
+
+         if (!(minDate > endDate || endDate > maxDate || startDate > endDate)){
+            this.setState({invalidEndDate : false})
+        }
+        else {
+            this.setState({invalidEndDate : true})
+        }
+    }
+
+    private handleStartDate(){
+        const startDate = new Date((document.getElementById("formBasicStartDate") as HTMLInputElement).value)
+        const endDate = new Date((document.getElementById("formBasicEndDate") as HTMLInputElement).value)
+        let maxDate: Date = new Date("3000-01-01")
+        let minDate: Date = new Date("1970-01-01")
+
+        if (!(startDate > endDate || startDate > maxDate || minDate > startDate)){
+            this.setState({invalidStartDate : false})
+        }
+        else {
+            this.setState({invalidStartDate : true})
+        }
+    }
+
     private handleValidity() {
         const button = document.getElementById("submitbutton") as HTMLInputElement | null;
         const projectName = (document.getElementById("formBasicProjectName") as HTMLInputElement).value
@@ -73,7 +123,8 @@ class ProjectCreate extends Component<ProjectCreateProp> {
         const endDate = new Date((document.getElementById("formBasicEndDate") as HTMLInputElement).value)
         const teamLeader = this.state.assignedToManager
 
-        if (/^\s/g.test(projectName) || projectName === '' || !/\d/g.test(startDate.toString()) || !/\d/g.test(endDate.toString()) || teamLeader?.name === '' || teamLeader?.id == null || /\d/g.test(teamLeader.name) || /\D/g.test(teamLeader.id.toString())){
+
+        if (/^\s/g.test(projectName) || projectName === '' || !/\d/g.test(startDate.toString()) || !/\d/g.test(endDate.toString()) || teamLeader?.name === '' || teamLeader?.id == null || /\d/g.test(teamLeader.name) || /\D/g.test(teamLeader.id.toString()) || this.state.invalidStartDate || this.state.invalidEndDate){
             button?.setAttribute('disabled', '')
         }
         else{
@@ -81,31 +132,38 @@ class ProjectCreate extends Component<ProjectCreateProp> {
         }
     }
 
-    handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
-        event.preventDefault();
+    handleFormSubmit = () =>{
+        //event.preventDefault();
         const projectName = (document.getElementById("formBasicProjectName") as HTMLInputElement).value
         const parentProject = this.state.selectedParentProject.id
         const startDate = new Date((document.getElementById("formBasicStartDate") as HTMLInputElement).value)
         const endDate = new Date((document.getElementById("formBasicEndDate") as HTMLInputElement).value)
         const teamLeader = this.state.assignedToManager.id
+        const tasks = this.state.tasks
 
         const post_data = {
             name: projectName,
             startDate: startDate,
             endDate: endDate,
             projectLeader: [teamLeader],
-            superProjectId: parentProject
+            superProjectId: parentProject,
+            tasks: tasks
         }
 
-        let apiHandler = new BaseApiHandler("posttest");
+        let apiHandler = new BaseApiHandler();
         apiHandler.post(`/api/project/creation/post`, {body:post_data}, (value) =>{
             console.log(value);
 
+
         })
+        this.setState({formSubmitted: true})
+        this.handleClose()
     }
 
     private informationRender():JSX.Element {
 
+        // @ts-ignore
+        // @ts-ignore
         return (
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                 <Row>
@@ -123,7 +181,7 @@ class ProjectCreate extends Component<ProjectCreateProp> {
                         <Tab.Content>
                             <Tab.Pane eventKey="first">
                                 <h1>Create a project</h1>
-                                <Form onSubmit={this.handleFormSubmit}>
+                                <Form>
                                     <Row>
                                         <Col>
                                             <Form.Group className="mb-3" controlId="formBasicProjectName" onChange={() => {this.handleValidity.call(this)}}>
@@ -164,15 +222,21 @@ class ProjectCreate extends Component<ProjectCreateProp> {
                                     </Row>
                                     <Row>
                                         <Col>
-                                            <Form.Group className="mb-3" controlId="formBasicStartDate" onChange={() => {this.handleValidity.call(this)}}>
+                                            <Form.Group className="mb-3" controlId="formBasicStartDate" onChange={() => {this.handleValidity.call(this); this.handleStartDate.call(this)}}>
                                                 <Form.Label>Start Date*</Form.Label>
-                                                <Form.Control type="date" placeholder="1/20/1970" />
+                                                <Form.Control type="date" placeholder="1/20/1970" min="1970-01-01" max="3000-01-01" isInvalid={this.state.invalidStartDate}/>
+                                                <Form.Control.Feedback type="invalid">
+                                                    {<p><b>Date is invalid</b></p>}
+                                                </Form.Control.Feedback>
                                             </Form.Group>
                                         </Col>
                                         <Col>
-                                            <Form.Group className="mb-3" controlId="formBasicEndDate" onChange={() => {this.handleValidity.call(this)}}>
+                                            <Form.Group className="mb-3" controlId="formBasicEndDate" onChange={() => {this.handleValidity.call(this); this.handleEndDate.call(this)}}>
                                                 <Form.Label>End Date*</Form.Label>
-                                                <Form.Control type="date" placeholder="1/20/1970" />
+                                                <Form.Control type="date" placeholder="1/20/1970" min="1970-01-01" max="3000-01-01" isInvalid={this.state.invalidEndDate} />
+                                                <Form.Control.Feedback type="invalid">
+                                                    {<p><b>Date is invalid</b></p>}
+                                                </Form.Control.Feedback>
                                             </Form.Group>
                                         </Col>
                                     </Row>
@@ -210,10 +274,27 @@ class ProjectCreate extends Component<ProjectCreateProp> {
                                             )}
                                         />
                                     </Form.Group>
-                                    <Col>
-                                        <Button variant="success" type="submit" disabled={true} id="submitbutton">Create Project</Button>
-                                    </Col>
+                                    <Button variant="success" id="submitbutton" onClick={this.handleShow}>Submit project</Button>
+                                    <Modal show={this.state.show} onHide={this.handleClose}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Confirm project</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>Check that you have filled out all required fields correctly *</Modal.Body>
+                                        <Modal.Body>Are you sure you want to create this project?</Modal.Body>
+                                        <Modal.Footer>
+                                            <Button variant="secondary" onClick={this.handleClose}>
+                                                Close
+                                            </Button>
+                                        <Button variant="success" type="submit" id="submitbutton" onClick={this.handleFormSubmit}>Create Project</Button>
+                                        </Modal.Footer>
+                                    </Modal>
                                 </Form>
+                                {this.state.formSubmitted ? (<Alert variant="success" id="alert">
+                                    <Alert.Heading>Project created!</Alert.Heading>
+                                    <p>
+                                        Your project has been successfully created, it can now be viewed under the <a href="/project/menu">project menu</a>
+                                    </p>
+                                </Alert>) : ""}
                             </Tab.Pane>
                             <Tab.Pane eventKey="second">
                                 <h3>Task list</h3>
