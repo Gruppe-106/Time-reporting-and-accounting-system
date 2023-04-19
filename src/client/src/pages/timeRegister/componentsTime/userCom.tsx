fdf55faf692ba5a0a40ba71cc8fba9380d39a81b
Christianbonde <cbje22@student.aau.dk>
2023-04-19 15:13:36 +0200

    Time changes are done and working

--- src/client/src/pages/timeRegister/componentsTime/userCom.tsx
+++ src/client/src/pages/timeRegister/componentsTime/userCom.tsx
@@ -148,7 +148,7 @@ class TimeSheetPage extends Component<TimeSheetProp, TimeSheetState> {
     const { stateRowData, offsetState } = this.state;
 
     let newValue = parseInt(value);
-    if(isNaN(newValue)) newValue = 0
+    if (isNaN(newValue)) newValue = 0
 
     let dates: string[] = [];
     getCurrentWeekDates(dates, offsetState);
@@ -160,12 +160,12 @@ class TimeSheetPage extends Component<TimeSheetProp, TimeSheetState> {
         rowData.objectData.map((item) => {
           let minutes = item.time % 60
           if (item.date === Date.parse(dates[index])) {
-            if (newValue === 0 && rowData) {
+            if (newValue + minutes === 0 && rowData) {
               rowData.objectData = rowData.objectData.filter(
                 (objItem) => objItem.date !== item.date
               );
             } else {
-              if(newValue < item.time /60){
+              if (newValue < item.time / 60) {
                 item.time = newValue * 60 + (minutes);
               } else {
                 item.time = newValue * 60 + (minutes);
@@ -178,6 +178,43 @@ class TimeSheetPage extends Component<TimeSheetProp, TimeSheetState> {
 
         if (!dateFound && newValue > 0) {
           rowData.objectData.push({ date: Date.parse(dates[index]), time: Math.max(0, newValue * 60) });
+        }
+
+        this.setState({ stateRowData });
+        break; // Break out of the map function if date matches or if a new object was pushed.
+      }
+    }
+    console.log(stateRowData)
+  }
+  private handleTimeSelectChange(index: number, value: string, data: TaskRowData | undefined) {
+    const { stateRowData, offsetState } = this.state;
+
+    let newValue = parseInt(value);
+
+    let dates: string[] = [];
+    getCurrentWeekDates(dates, offsetState);
+
+    for (const key of Array.from(stateRowData.keys())) {
+      let rowData = stateRowData.get(key);
+      if (data && rowData && data.taskId === rowData.taskId) {
+        let dateFound: boolean = false;
+        rowData.objectData.map((item) => {
+          let hoursInMinutes = Math.floor(item.time / 60) * 60
+          if (item.date === Date.parse(dates[index])) {
+            if (newValue + hoursInMinutes === 0 && rowData) {
+              rowData.objectData = rowData.objectData.filter(
+                (objItem) => objItem.date !== item.date
+              );
+            } else {
+              item.time = newValue + hoursInMinutes;
+            }
+            dateFound = true;
+          }
+          return item; // just to return something
+        });
+
+        if (!dateFound && newValue > 0) {
+          rowData.objectData.push({ date: Date.parse(dates[index]), time: Math.max(0, newValue) });
         }
 
         this.setState({ stateRowData });
@@ -322,13 +359,14 @@ class TimeSheetPage extends Component<TimeSheetProp, TimeSheetState> {
               return (
                 <td key={index}>
                   <InputGroup size="sm">
-                    <Form.Control type="number" placeholder="0" value={Math.floor(arr[index]/ 60)} onChange={(e) => this.handleTimeChange(index, e.target.value, data)} />
+                    <Form.Control type="number" placeholder="0" value={Math.floor(arr[index] / 60)} onChange={(e) => this.handleTimeChange(index, e.target.value, data)} />
                     <InputGroup.Text id={`basic-addon-${index}`}>:</InputGroup.Text>
                     <Form.Select
                       style={{ fontSize: '14px', border: '1px solid #ccc', borderRadius: '0 4px 4px 0', fontFamily: 'Helvetica', color: "#212529" }}
                       className="myFormSelect"
                       bsPrefix="myFormSelect"
-                      defaultValue={arr[index] % 60}>
+                      defaultValue={arr[index] % 60}
+                      onChange={(e) => this.handleTimeSelectChange(index, e.target.value, data)}>
                       <option value={0}>0</option>
                       <option value={15}>15</option>
                       <option value={30}>30</option>
