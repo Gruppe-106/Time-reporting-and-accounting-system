0ffc5dad283acbc1324e637bcfee449e4937cba5
Mikkel Andersen <108178976+Oragif@users.noreply.github.com>
2023-04-19 15:06:26 +0200

    Started Auth Test

---
+++ src/server/serverTest/authEndpoint.test.ts
@@ -0,0 +1,10 @@
+import BaseApiHandler from "../../client/src/network/baseApiHandler";
+import {headers} from "./testBaseConfig";
+
+const apiHandler = new BaseApiHandler("http://localhost:8080");
+
+describe("Auth API", () => {
+   describe("POST API", () => {
+
+   });
+});
