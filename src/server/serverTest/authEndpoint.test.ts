067f4aa092f224d43120ab01da809de28081124e
Mikkel Andersen <108178976+Oragif@users.noreply.github.com>
2023-04-20 09:16:53 +0200

    Auth test

--- src/server/serverTest/authEndpoint.test.ts
+++ src/server/serverTest/authEndpoint.test.ts
@@ -5,6 +5,18 @@ const apiHandler = new BaseApiHandler("http://localhost:8080");
 
 describe("Auth API", () => {
    describe("POST API", () => {
+      test("Auth Fail", () => {
+         apiHandler.post("/api/auth", {}, (value) => {
+            expect(value["status"]).toBe(404);
+            expect(value["data"]["success"]).toMatch("false");
+         });
+      });
 
+      test("Auth Success", () => {
+         apiHandler.post("/api/auth", {headers: headers}, (value) => {
+            expect(value["status"]).toBe(200);
+            expect(value["data"]["success"]).toMatch("true");
+         });
+      });
    });
 });
