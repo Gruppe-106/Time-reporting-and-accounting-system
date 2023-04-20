5b67ac973c5489d83b475a1e8f7ac10ff6028862
Andreas <ahumme22@student.aau.dk>
2023-04-20 12:17:54 +0200

    maybe fixing

--- src/server/apiEndpoints/dataDeleteEndpoints/userDeleteEndpoint.ts
+++ src/server/apiEndpoints/dataDeleteEndpoints/userDeleteEndpoint.ts
@@ -1,27 +1,36 @@
 import GetEndpointBase from "../getEndpointBase";
-import {Request, Response} from "express";
-import {MySQLResponse} from "../../database/mysqlHandler";
+import { Request, Response } from "express";
+import { MySQLResponse } from "../../database/mysqlHandler";
 
 class UserDeleteEndpoint extends GetEndpointBase {
     allowedColumns: string[];
     requiredRole: number = 4;
 
     async getData(requestValues: string[], primaryKey: string, keyEqual?: string[], data?: string[]): Promise<object[]> {
-        let response: MySQLResponse = await this.mySQL.remove("USERS", [{
-            column: "id", equals: keyEqual
-        }]);
-        if (response.error !== null) {
-            return Promise.resolve([{error: "Failed to delete user"}])
+        console.log(keyEqual)
+        if (!keyEqual || keyEqual.length === 0) {
+            return Promise.resolve([{ error: "No user ID provided" }]);
         }
 
-        return Promise.resolve([]);
+        let response: MySQLResponse = await this.mySQL.remove("USERS", [{
+            column: "id", equals: keyEqual // Pass the first element of the array
+        }]);
+        if (response.error !== null) {
+            return Promise.resolve([{ error: "Failed to delete user" }])
+        }
+
+        return Promise.resolve([{ success: "User deleted successfully" }]);
     }
 
-    public getRoute(req:Request, res:Response, primaryKey:string = "id", requestKeysName:string = "user") {
+
+
+    public getRoute(req: Request, res: Response, primaryKey: string = "id", requestKeysName: string = "user") {
         let requestKeys: string[] = this.urlParamsConversion(req.query[requestKeysName], false, true, res);
         if (requestKeys === undefined) { return this.badRequest(res, req); }
 
+        // Pass requestKeys as an array for the keyEqual parameter
         this.processRequest(req, undefined, primaryKey, requestKeys).then((data) => {
+
             if (!res.writableEnded) {
                 res.setHeader('Content-Type', 'application/json');
                 res.status(data.status).json(data);
@@ -29,6 +38,10 @@ class UserDeleteEndpoint extends GetEndpointBase {
 
         })
     };
+
+
+
 }
+
 
 export default UserDeleteEndpoint;
