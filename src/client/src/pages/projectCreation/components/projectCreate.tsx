7d826dbf936a9781b73c6fd7c450b683be9078bb
KanzanKohi <agredder@gmail.com>
2023-04-25 09:25:45 +0200

    simplified cursed if statement

--- src/client/src/pages/projectCreation/components/projectCreate.tsx
+++ src/client/src/pages/projectCreation/components/projectCreate.tsx
@@ -157,7 +157,15 @@ class ProjectCreate extends Component<ProjectCreateProp> {
         const endDate = new Date((document.getElementById("formBasicEndDate") as HTMLInputElement).value)
         const teamLeader = this.state.assignedToManager
 
-        if (/^\s/g.test(projectName) || projectName === '' || !/\d/g.test(startDate.toString()) || !/\d/g.test(endDate.toString()) || teamLeader?.name === '' || teamLeader?.id == null || /\d/g.test(teamLeader.name) || /\D/g.test(teamLeader.id.toString()) || this.state.invalidStartDate || this.state.invalidEndDate){
+        let invalidProjectName = /^\s/g.test(projectName) || projectName === ''
+        let invalidDate = !/\d/g.test(startDate.toString()) || !/\d/g.test(endDate.toString())
+        let invalidTeamLeader = teamLeader?.name === '' || teamLeader?.id == null || /\d/g.test(teamLeader.name) || /\D/g.test(teamLeader.id.toString())
+
+        if (invalidProjectName
+            || invalidDate
+            || invalidTeamLeader
+            || this.state.invalidStartDate
+            || this.state.invalidEndDate){
             button?.setAttribute('disabled', '')
         }
         else{
