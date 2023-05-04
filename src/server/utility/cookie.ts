86a75076e29acb54c0f85a19beb41aedd6eef7b4
Oragif <Mikkelhelsing1@gmail.com>
2023-05-04 21:52:18 +0200

    Cookies are not send to server anymore

--- src/server/utility/cookie.ts
+++
@@ -1,8 +0,0 @@
-export function getCookies(cookieString: string): Map<string, string> {
-    let cookies: Map<string, string> = new Map<string, string>();
-    for (const cookie of cookieString.split("; ")) {
-        let data = cookie.trim().split('=')
-        cookies.set(data[0], data[1]);
-    }
-    return cookies;
-}
